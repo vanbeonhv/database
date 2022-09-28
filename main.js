0640b67f6dbf52a21ce780782f455dd8b144a627
Phantom Lancer <nguyenvannhv26@gmail.com>
2022-09-29 00:18:00 +0700

    update users data

--- main.js
+++ main.js
@@ -13,15 +13,15 @@ server.get("/echo", (req, res) => {
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-  if (req.method === "POST") {
-    req.body.createdAt = Date.now();
-    // req.body.updateAt = Date.now();
-  }
-  // Continue to JSON Server router
-  next();
-});
+// server.use(jsonServer.bodyParser);
+// server.use((req, res, next) => {
+//   if (req.method === "POST") {
+//     req.body.createdAt = Date.now();
+// req.body.updateAt = Date.now();
+//   }
+//   // Continue to JSON Server router
+//   next();
+// });
 
 // Use default router
 server.use("/api", router);
